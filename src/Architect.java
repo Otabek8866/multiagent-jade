4a818d0a269d31172b9fac3677ef5f490484d77d
Otabek8866 <sobirovotabek1995@gmail.com>
2021-11-04 12:33:12 +0100

    updated Architect.java

--- src/Architect.java
+++ src/Architect.java
@@ -18,6 +18,8 @@ import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,12 +27,24 @@ import java.net.*;
  */
 public class Architect extends GuiAgent {
 
+    // for local host
     ContainerController containerController;
     Runtime runtime;
     Profile agentProfile;
     AgentController controller;
+    
+//    // for remote host
+//    ContainerController remote_containerController;
+//    Runtime remote_runtime;
+//    Profile remote_agentProfile;
+//    AgentController remote_controller;
+//    
+    // creating a GUI
     private AgentGUI master_gui;
+    // Architect IP addresss
+    String my_ip_address;
 
+    
     protected void setup() {
 
         master_gui = new AgentGUI(this);
@@ -56,6 +70,7 @@ public class Architect extends GuiAgent {
     protected void attack() {
         runtime = Runtime.instance();
 
+        // Local host
         // prepare the settings for the platform that we're going to connect to
         agentProfile = new ProfileImpl();
         agentProfile.setParameter(Profile.MAIN_HOST, "localhost");
@@ -64,6 +79,19 @@ public class Architect extends GuiAgent {
 
         // create the agent container
         containerController = runtime.createAgentContainer(agentProfile);
+        
+        // Remotehost
+//        remote_runtime = Runtime.instance();
+//
+//        // prepare the settings for the platform that we're going to connect to
+//        remote_agentProfile = new ProfileImpl(false);
+//        remote_agentProfile.setParameter(Profile.PLATFORM_ID, "192.168.26.188:1099/JADE");
+//        remote_agentProfile.setParameter(Profile.MAIN_HOST, "192.168.26.188");
+//        remote_agentProfile.setParameter(Profile.MAIN_PORT, "1099");
+//        remote_agentProfile.setParameter(Profile.CONTAINER_NAME, "Remote_Agent_Smiths");
+//
+//        // create the agent container
+//        remote_containerController = remote_runtime.createAgentContainer(remote_agentProfile);
 
         int total_num_agent = master_gui.total_num_agent;
         int session_time = master_gui.session_time;
@@ -71,7 +99,7 @@ public class Architect extends GuiAgent {
         InetAddress target_ip = master_gui.target_ip;
         int port = master_gui.port;
 
-        this.createAgent(total_num_agent, session_time, message, target_ip, port);
+        createAgent(total_num_agent, session_time, message, target_ip, port);
 
     }
 
@@ -85,20 +113,31 @@ public class Architect extends GuiAgent {
 
     public void createAgent(int total_num_agent, int session_time, String message, InetAddress target_ip, int port) {
 
-        Object[] arguments = new Object[4];
+        Object[] arguments = new Object[5];
         arguments[0] = target_ip;
         arguments[1] = port;
         arguments[2] = session_time;
         arguments[3] = message;
+        
+        try {
+            arguments[4] = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Architect.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         master_gui.console_text_setter(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         master_gui.console_text_setter(" Target  ==>   IP: " + target_ip.toString() + "     Port: " + port);
         master_gui.console_text_setter(" Number of Agnets: " + total_num_agent + "     Message: " + message + "\n");
 
         try {
-            for (int i = 1; i < total_num_agent + 1; i++) {
+            for (int i = 1; i <= total_num_agent + 1; i++) {
+                // for local host
                 controller = containerController.createNewAgent("AgentSmith" + i, "SimpleAgent", arguments);
                 controller.start();
+                
+//                // for remote host
+//                remote_controller = remote_containerController.createNewAgent("Remote_AgentSmith" + i, "SimpleAgent", arguments);
+//                remote_controller.start();
             }
         } catch (StaleProxyException e) {
             e.printStackTrace();
@@ -110,6 +149,7 @@ public class Architect extends GuiAgent {
     public void killContainer() {
         try {
             containerController.kill();
+            //remote_containerController.kill();
         } catch (StaleProxyException e) {
             e.printStackTrace();
             System.out.println("Problem Architect: " + e.toString());
